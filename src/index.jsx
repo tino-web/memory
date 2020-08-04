f66ffa1089a2aa6824d1efa57270200a8ce861bd
Tino <rijnders.martijn@gmail.com>
2020-08-04 17:28:26 +0700

    Change to HashRouter for GH Pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from '@components/App/App';
 import '@assets/css/style.css';
 import { ContextProvider } from '@context/gameContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter basename='/'>
       <ContextProvider>
         <App />
       </ContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
